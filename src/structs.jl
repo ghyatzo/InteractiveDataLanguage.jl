2c82490676ca6be5f67639ea54ae758ac2e2b5c7
Camillo Schenone <camillo.schenone@gmail.com>
2025-02-06 13:32:31 +0100

    clearer parameters names, and some extra helper methods

--- src/structs.jl
+++ src/structs.jl
@@ -15,6 +15,9 @@ end
 value(t::NestedStructTag) = t.s
 gettag(t::NestedStructTag) = value(t)
 
+
+
+
 struct ArrayTag{T, N} <: StructTag{T}
 	arr::IDLArray{T, N}
 	offset::UInt8
@@ -26,6 +29,9 @@ end
 value(t::ArrayTag) = t.arr
 gettag(t::ArrayTag) = value(t)
 
+
+
+
 struct ScalarTag{T} <: StructTag{T}
 	ptr::Ptr{T}
 	offset::UInt8
@@ -35,7 +41,10 @@ value(t::ScalarTag) = t.ptr
 gettag(t::ScalarTag) = unsafe_load(value(t))
 gettag(t::ScalarTag{IDL_STRING}) = convert(String, value(t))
 
+
 tagoffset(t::StructTag) = t.offset
+
+
 
 _extract_tag_info(sref::IDL_SREF, i::Int) = begin
 	_tagvar = Ref{Ptr{IDL_VARIABLE}}()
@@ -46,6 +55,7 @@ _extract_tag_info(sref::IDL_SREF, i::Int) = begin
 
 	return _tagvar[], offset
 end
+
 
 function (::Type{StructTag})(sref::IDL_SREF, i::Int, _data)
 
@@ -67,10 +77,25 @@ function (::Type{StructTag})(sref::IDL_SREF, i::Int, _data)
 	return ScalarTag{T}(_data, offset)
 end
 
-struct IDLStruct{name, tagnames, types} <: GenericIDLStruct
+
+
+struct IDLStruct{N, L, T} <: GenericIDLStruct
 	sref::IDL_SREF
 	tags::Tuple{Vararg{StructTag}}
 	ptr::Ptr{UInt8}
+end
+
+Base.propertynames(::IDLStruct{N, L, T}) where {N, L, T} = L
+Base.nameof(::IDLStruct{N, L, T}) where {N, L, T} = N
+ntags(::IDLStruct{N, L, T}) where {N, L, T} = length(L)
+tags(::IDLStruct{N, L, T}) where {N, L, T} = L
+tagtypes(::IDLStruct{N, L, T}) where {N, L, T} = fieldtypes(T)
+tagtype(s::IDLStruct, i::Integer) = tagtypes(s)[i]
+tagtype(s::IDLStruct, t::Symbol) = begin
+	i = findfirst(==(t), tags(s))
+	isnothing(i) && throw(ErrorException("Struct has no tag '$t'"))
+
+	tagtype(s, i)
 end
 
 function IDLStruct(sref::IDL_SREF, inherited_data = C_NULL)
@@ -101,15 +126,17 @@ function IDLStruct(sref::IDL_SREF, inherited_data = C_NULL)
 	return IDLStruct{Symbol(stname), tags, typeof(values)}(sref, values, _data)
 end
 
-function IDLStruct(
-	s::IDLStruct{name, tags, types},
-	dataptr::Ptr{UInt8}
-) where {name, tags, types}
 
-	tagtypes = fieldtypes(types)
-	values = ntuple(length(tags)) do i
+
+function IDLStruct(
+	s::IDLStruct{N, L, T}, dataptr::Ptr{UInt8}
+) where {N, L, T}
+
+	tagtypes = fieldtypes(T)
+	values = ntuple(length(L)) do i
 		type = tagtypes[i]
 		tag = s.tags[i]
+
 		type isa ScalarTag &&
 			return ScalarTag{eltype(tag)}(dataptr, offset(tag))
 
@@ -120,23 +147,25 @@ function IDLStruct(
 			NestedStructTag(value(tag).sref, dataptr, offset(tag))
 	end
 
-	return IDLStruct{name, tags, types}(s.sref, values, dataptr)
+	return IDLStruct{N, L, T}(s.sref, values, dataptr)
 end
 
-function IDLStruct{name, tags, types}(values...) where {name, tags, types}
-	name isa Symbol ||
-		throw(TypeError(:IDLStruct, Type{Symbol}, name))
 
-	tags isa Tuple{Vararg{Symbol}} ||
-		throw(TypeError(:IDLStruct, Tuple{Vararg{Symbol}}, tags))
 
-	types <: Tuple ||
-		throw(TypeError(:IDLStruct, Tuple{StructTag}, types))
+function IDLStruct{N, L, T}(values...) where {N, L, T}
+	N isa Symbol ||
+		throw(TypeError(:IDLStruct, Type{Symbol}, N))
 
-	length(tags) == length(values) ||
+	L isa Tuple{Vararg{Symbol}} ||
+		throw(TypeError(:IDLStruct, Tuple{Vararg{Symbol}}, L))
+
+	T <: Tuple ||
+		throw(TypeError(:IDLStruct, Tuple{StructTag}, T))
+
+	length(L) == length(values) ||
 		throw(ArgumentError("Tag names and values must have the same length."))
 
-	eltypes = fieldtypes(types)
+	eltypes = fieldtypes(T)
 	length(eltypes) == length(values) ||
 		throw(ArgumentError("Tag types and values must have the same length."))
 
@@ -144,35 +173,33 @@ function IDLStruct{name, tags, types}(values...) where {name, tags, types}
 		tag isa type || throw(TypeError(:IDLStruct, type, tag))
 	end
 
-	return IDLStruct{name, tags, types}(values, C_NULL) #todo...
+	return IDLStruct{N, L, T}(values, C_NULL) #FIXME: How to include julia defined data?
 end
 
-IDLStruct{name, tags}(values...) where {name, tags} =
-	IDLStruct{name, tags, typeof(values)}(values, C_NULL)
+IDLStruct{N, L}(values...) where {N, L} =
+	IDLStruct{N, L, typeof(values)}(values, C_NULL)
 
-IDLStruct{tags, types}(values...) where {tags, types <: Tuple} =
-	IDLStruct{Symbol(""), tags, types}(values, C_NULL)
+IDLStruct{L, T}(values...) where {L, T <: Tuple} =
+	IDLStruct{Symbol(""), L, T}(values, C_NULL)
 
-IDLStruct{tags}(values...) where {tags} =
-	IDLStruct{Symbol(""), tags, typeof(values)}(values, C_NULL)
+IDLStruct{L}(values...) where {L} =
+	IDLStruct{Symbol(""), L, typeof(values)}(values, C_NULL)
 
-Base.propertynames(::IDLStruct{name, tags, types}) where {name, tags, types} = tags
-Base.nameof(::IDLStruct{name, tags, types}) where {name, tags, types} = name
-ntags(::IDLStruct{name, tags, types}) where {name, tags, types} = length(tags)
 
-function Base.getproperty(s::IDLStruct{name, tags, types}, f::Symbol) where {name, tags, types}
+
+function Base.getproperty(s::IDLStruct{N, L, T}, f::Symbol) where {N, L, T}
 	f in fieldnames(IDLStruct) && return getfield(s, f)
 
-	i = findfirst(==(f), tags)
+	i = findfirst(==(f), L)
 	isnothing(i) && throw(ErrorException("Struct has no tag '$f'"))
 
 	return gettag(s.tags[i])
 end
 
-function Base.show(io::IO, s::IDLStruct{name, tags, types}) where {name, tags, types}
-	if name == Symbol()
+function Base.show(io::IO, s::IDLStruct{N, L, T}) where {N, L, T}
+	if N == Symbol()
 		print(io, "IDLStruct{")
-		tag1, rtags... = tags
+		tag1, rtags... = L
 		val1, rvalues... = s.tags
 		print(io, "$tag1:$val1")
 		for (tag, value) in zip(rtags, rvalues)
@@ -180,7 +207,7 @@ function Base.show(io::IO, s::IDLStruct{name, tags, types}) where {name, tags, t
 		end
 	else
 		print(io, "IDLStruct{$name")
-		for (tag, value) in zip(tags, s.tags)
+		for (tag, value) in zip(L, s.tags)
 			print(io, ", $tag:$value")
 		end
 	end
