66d01ce7b0963cf8e4571d4d5d6532ba9b35956f
Camillo Schenone <camillo.schenone@gmail.com>
2025-02-06 13:31:15 +0100

    rewrite the logic for struct of arrays, and remove superfluous function.

--- src/common.jl
+++ src/common.jl
@@ -13,7 +13,6 @@
 # end
 
 
-
 ###=== IDL -> JULIA ===###
 
 function get_var end
@@ -37,213 +36,76 @@ function get_var(_var::Ptr{IDL_VARIABLE})
 		error("File Variables not yet implemented")
 	end
 
-	if (var_f & IDL_V_STRUCT) != 0
-		# IDL Structures are really only Arrays of structures with only one element.
-		# IDL_V_STRUCT implies IDL_V_ARR so it must be before
-		# to distinguish between struct_array and array only.
-		return _get_idl_struct_array(_var)
-	end
-
 	if (var_f & IDL_V_ARR) != 0
-		_arr = unsafe_load(_var.value.arr)
-		var_t = unsafe_load(_var.type)
-		array = IDLArray(_arr, var_t)
+		arr = unsafe_load(unsafe_load(_var.value.arr))
+		ndims = arr.n_dim
+		nelts = arr.n_elts
+
+		if (var_f & IDL_V_STRUCT) != 0
+			# IDL_V_STRUCT implies IDL_V_ARR
+			# IDL Structures are really only Arrays of structures with only one element.
+			sref = unsafe_load(_var.value.s)
+
+			s = IDLStruct(sref)
+			struct_t = typeof(s)
+
+			if nelts > 1
+				#var.value.s.arr == var.value.arr (C Union)
+				elsize = arr.elt_len
+				array = Array{struct_t, ndims}(undef, nelts)
+				array[1] = s
+				for i in 2:nelts
+					offset = elsize * (i-1)
+					array[i] = IDLStruct(s, s.ptr + offset)
+				end
+				return array
+			else
+				return s
+			end
+		end
+		jltype = jl_type(var_t)
+		array = IDLArray{jltype, ndims}(arr, C_NULL)
 		return PermutedDimsArray(array, dimsperm(ndims(array)))
 	end
 
 	return get_scalarvar(_var)
 end
 
+# function _get_idl_struct_array(_var::Ptr{IDL_VARIABLE})
+# 	# Array of structs in IDL are a mix between SoA and AoS...
+# 	# The memory layout is like an AoS with all values inlined in the same array
+# 	# in order, but the first struct defines the type of struct all other structs
+# 	# will have to be consistent with.
+# 	arr = _var.value.arr |> unsafe_load |> unsafe_load
+# 	sref = _var.value.s |> unsafe_load
 
+# 	# In IDL there is no real distinction between a struct
+# 	# and an array of structures with only one element.
+# 	# So we split here the logic.
+# 	N = arr.n_elts
+# 	struct_nt = IDLStruct(sref)
+# 	if N == 1
+# 		return struct_nt
+# 	else
+# 		# array of structs in IDL can only be uniform with the same struct.
+# 		# So we can construct things
+# 		r = typeof(struct_nt)[]
+# 		elsize = arr.elt_len # size of the struct
+# 		arrlen = arr.arr_len
 
-struct StructTag
-	name::String
-	offset::IDL_MEMINT
-	eltype::IDL_MEMINT
-	arrinfo::Union{Nothing, IDL_ARRAY}
-	structinfo::Union{Nothing, IDL_SREF}
+# 		_data = arr.data
+# 		for i in 0:N-1
+# 			_struct_offset = _data + (elsize * i)
+# 			push!(r, makestruct(sref, _struct_offset))
+# 		end
 
-	StructTag(sref::IDL_SREF, i::Integer) = begin
-		@assert i > 0 "the index i is 1 based."
-
-		__tagvar = Ref{Ptr{IDL_VARIABLE}}()
-		offset = IDL_StructTagInfoByIndex(sref.sdef, i-1, IDL_MSG_RET, __tagvar)
-		if offset == -1
-			throw(ArgumentError("The structure does not have an $i-th tag."))
-		end
-
-		tname = tagname(sref, i)
-		tagvar = unsafe_load(__tagvar[])
-		tageltype = tagvar.type
-
-		tag_array_info = nothing
-		if tagvar.flags & IDL_V_ARR != 0
-			tag_arr = unsafe_load(tagvar.value.arr)
-			tag_array_info = tag_arr
-		end
-
-		if tageltype == IDL_TYP_STRUCT
-			tag_struct_info = tagvar.value.s
-		end
-
-		new(tname, offset, tageltype, tag_array_info, tag_struct_info)
-	end
-end
-
-
-isarray(tag::StructTag) = !isnothing(tag.arrinfo)
-isstruct(tag::StructTag) = !isnothing(tag.structinfo)
-isscalar(tag::StructTag) = !isarray(tag) && !isstruct(tag)
-Base.eltype(tag::StructTag) = jl_type(tag.eltype)
-
-name(sref::IDL_SREF) = begin
-	__struct_name = Ref{Ptr{Cchar}}()
-	IDL_StructTagNameByIndex(sref.sdef, C_NULL, IDL_MSG_RET,  __struct_name)
-	return unsafe_string(__struct_name[])
-end
-
-ntags(sref::IDL_SREF) = IDL_StructNumTags(sref.sdef)
-tagname(sref::IDL_SREF, i::Integer) = IDL_StructTagNameByIndex(sref.sdef, i-1, 0, C_NULL) |> unsafe_string
-tagnames(sref::IDL_SREF) = ntuple(i -> Symbol(tagname(sref, i)), ntags(sref))
-
-
-struct IDLStruct
-	name::String
-	tags::Vector{StructTag}
-	sdef::IDL_SREF
-	data::Ptr{Cuchar}
-
-	function IDLStruct(sref::IDL_SREF, data::Ptr{Cuchar})
-	end
-end
-
-function _get_idl_struct(sref::IDL_SREF, _parentdata = C_NULL)
-
-	struct_name = name(sref)
-	if struct_name != "<Anonymous>" # this string is hardcoded on the IDL side.
-		struct_nt = (; __name__ = struct_name) # dunder keys to avoid conflicts?
-	else
-		struct_nt = NamedTuple()
-	end
-	# this workaround is due to the fact that IDL structures store their data inlined
-	# in the same array also for nested structures.
-	# We use parentdata as an offset for the inner structure tag's offsets
-	_data = _parentdata == C_NULL ? unsafe_load(sref.arr).data : _parentdata
-
-	for i in 1:ntags(sref)
-		tag = StructTag(sref, i)
-		_tagdata = _data + tag.offset
-
-		if isstruct(tag)
-
-			# The data of the innerstructures is inlined in the same array as the main struct!
-			# we call recursively into the sub structure, passing the
-			# offset to the begin of the inner structure.
-			struct_nt = (; struct_nt..., Symbol(tag.name) => _get_idl_struct(tag.structinfo, _tagdata))
-
-		elseif isarray(tag)
-
-			array = IDLArray(tag.arrinfo, tag.eltype, _tagdata)
-			struct_nt = (; struct_nt..., Symbol(tag.name) => PermutedDimsArray(array, dimsperm(ndims(array))))
-
-		else
-
-			_value = Ptr{jl_type(tag.eltype)}(_tagdata)
-			tagvalue = unsafe_load(_value)
-			struct_nt = (; struct_nt..., Symbol(tag.name) => tagvalue)
-
-		end
-	end
-
-	struct_nt
-end
-
-function _get_idl_struct_array(_var::Ptr{IDL_VARIABLE})
-	# Array of structs in IDL are a mix between SoA and AoS...
-	# The memory layout is like an AoS with all values inlined in the same array
-	# in order, but the first struct defines the type of struct all other structs
-	# will have to be consistent with.
-	arr = _var.value.arr |> unsafe_load |> unsafe_load
-	sref = _var.value.s |> unsafe_load
-
-	# In IDL there is no real distinction between a struct
-	# and an array of structures with only one element.
-	# So we split here the logic.
-	N = arr.n_elts
-	struct_nt = _get_idl_struct(sref)
-	if N == 1
-		return struct_nt
-	else
-		# array of structs in IDL can only be uniform with the same struct.
-		# So we can construct things
-		r = typeof(struct_nt)[]
-		elsize = arr.elt_len # size of the struct
-		arrlen = arr.arr_len
-
-		_data = arr.data
-		for i in 0:N-1
-			_struct_offset = _data + (elsize * i)
-			push!(r, _get_idl_struct(sref, _struct_offset))
-		end
-
-		return r
-	end
-end
-
-swaprowcol(dims, n) = begin
-	nc, nr, r... = dims
-	(nr, nc, r...)[1:n]
-end
-
-dimsperm(n) = ntuple(n) do i
-	n == 1 && return 1
-	i == 1 && return 2
-	i == 2 && return 1
-	return i
-end
-
-struct IDLArray{T, N} <: AbstractArray{T, N}
-	meta::IDL_ARRAY
-	dataoverride::Ptr{Cuchar}
-
-	function IDLArray(idl_array::IDL_ARRAY, type::Integer, offset=C_NULL)
-		flags = idl_array.flags
-		ndims = idl_array.n_dim % Int
-
-		if flags & IDL_A_FILE != 0
-			throw(ArgumentError("memory mapped arrays are not supported"))
-		end
-
-		new{jl_type(type), ndims}(idl_array, offset)
-	end
-end
-IDLArray(_arr::Ptr{IDL_ARRAY}, args...) = IDLArray(unsafe_load(_arr), args...)
-
-dataptr(X::IDLArray{T, N}) where {T,N} =
-	X.dataoverride == C_NULL ? Ptr{T}(X.meta.data) : Ptr{T}(X.dataoverride)
-
-Base.IndexStyle(::IDLArray) = IndexLinear()
-
-Base.size(X::IDLArray{T, N}) where {T,N} = X.meta.dim[1:N]
-
-Base.eltype(::IDLArray{T, N}) where {T,N} = T
-
-Base.getindex(X::IDLArray{T, N}, i) where {T,N} =
-	unsafe_load(dataptr(X), i)
-
-Base.setindex!(X::IDLArray{T, N}, v, i) where {T,N} =
-	unsafe_store!(dataptr(X), v, i)
-
-Base.getindex(X::IDLArray{IDL_STRING}, i::Integer) =
-	IDL_STRING_STR(unsafe_load(dataptr(X), i))
-
-Base.setindex!(X::IDLArray{IDL_STRING}, s::AbstractString, i::Integer) =
-	IDL_StrStore(dataptr(X) + (sizeof(IDL_STRING) * (Int(i)-1)), s)
+# 		return r
+# 	end
+# end
 
 
 function get_scalarvar(_var::Ptr{IDL_VARIABLE})
-	var_t = unsafe_load(_var.type)
-	var_f = unsafe_load(_var.flags)
+	var_f, var_t = varinfo(_var)
 
 	(var_f & IDL_V_NOT_SCALAR) != 0 &&
 		error("the variable is not scalar!")
@@ -274,12 +136,6 @@ end
 
 
 function put_var end
-
-# References:
-# https://github.com/tk3369/julia-notebooks/blob/master/ccall%20-%20using%20cconvert%20and%20unsafe_convert.ipynb
-# https://discourse.julialang.org/t/how-to-keep-a-reference-for-c-structure-to-avoid-gc/9310/25
-# https://discourse.julialang.org/t/memory-management-and-packagecompiler-libraries/72980/7
-# https://discourse.julialang.org/t/cconvert-and-unsafe-convert-with-immutable-struct-containing-a-pointer/124479/5
 
 # Structs in IDL can be named or anonymous.
 # named structs are simply user defined structs. That must be isbitstype.
