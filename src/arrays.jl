ab29979007e419319177a2aec658c393fe607a44
Camillo Schenone <camillo.schenone@gmail.com>
2025-02-08 18:01:15 +0100

    Completely reworked the IDLArray structure. Now is just a view over an idl variable array. It automatically detects when the data is freed from IDL and invalidates the julia variable to avoid segfaults or UB.
    Now included the behaviour of PermutedArraysDims, built-in.

--- src/arrays.jl
+++ src/arrays.jl
@@ -1,7 +1,3 @@
-swaprowcol(dims, n) = begin
-	nc, nr, r... = dims
-	(nr, nc, r...)[1:n]
-end
 
 dimsperm(n) = ntuple(n) do i
 	n == 1 && return 1
@@ -10,28 +6,88 @@ dimsperm(n) = ntuple(n) do i
 	return i
 end # (1,) or (2,1,3,4...). IDL swaps only the first two dimensions
 
-struct IDLArray{T, N} <: AbstractArray{T, N}
-	meta::IDL_ARRAY
+
+mutable struct IDLArray{T, N} <: AbstractArray{T, N}
+	_arr::Ptr{IDL_ARRAY}
 	dataoverride::Ptr{Cuchar}
+	_customcb::Base.CFunction
+
+	function IDLArray(v::IDLVariable, inheriteddata = C_NULL)
+		isarray(v) || throw(ArgumentError("The variable must point to an IDL Array."))
+
+		_arr = unsafe_load(v._v.value.arr)
+		N = unsafe_load(_arr.n_dim) % Int
+		T = typeof(v)
+
+		x = new{T, N}(_arr, inheriteddata)
+
+		# The default callback invalidates the IDLArray
+		x._customcb = @cfunction($((_p::Ptr{Cuchar}) -> begin
+			setfield!(x, :_arr, Ptr{IDL_ARRAY}(C_NULL))
+			nothing
+		end), Nothing, (Ptr{Cuchar},))
+
+		unsafe_store!(_arr.free_cb, Base.unsafe_convert(Ptr{Cvoid}, x._customcb))
+
+		return x
+	end
+end
+isvalidref(X::IDLArray) = getfield(X, :_arr) != C_NULL
+Base.getproperty(X::IDLArray, f::Symbol) = begin
+	f == :_arr && return isvalidref(X) ? getfield(X, :_arr) : throw(UndefRefError())
+	getfield(X, f)
 end
 
-function IDLArray{T}(arr::IDL_ARRAY, inheriteddata = C_NULL) where T
-	return IDLArray{T, Int(arr.n_dim)}(arr, inheriteddata)
+Base.isassigned(X::IDLArray, ::Integer) = isvalidref(X)
+Base.isassigned(X::IDLArray, I...) = isvalidref(X)
+
+_dataptr(X::IDLArray{T, N}) where {T, N} = begin
+	X.dataoverride == C_NULL ? Ptr{T}(unsafe_load(X._arr.data)) : Ptr{T}(X.dataoverride)
 end
 
-_dataptr(X::IDLArray{T, N}) where {T, N} =
-	X.dataoverride == C_NULL ? Ptr{T}(X.meta.data) : Ptr{T}(X.dataoverride)
-
+setcallback!(X::IDLArray, cb) = begin
+	newcb = @cfunction($cb, Nothing, (Ptr{Cuchar},))
+	X._customcb = newcb
+	unsafe_store!(X._arr.free_cb, Base.unsafe_convert(Ptr{Cvoid}, X._customcb))
+end
 Base.IndexStyle(::IDLArray) = IndexLinear()
-Base.size(X::IDLArray{T, N}) where {T, N} = X.meta.dim[1:N]
+Base.size(X::IDLArray{T, N}) where {T, N} = unsafe_load(X._arr.dim)[1:N]
 Base.eltype(::IDLArray{T, N}) where {T, N} = T
-Base.length(x::IDLArray) = x.meta.n_elts
+Base.length(X::IDLArray) = unsafe_load(X._arr.n_elts)
 
 
-Base.getindex(X::IDLArray, i) = unsafe_load(_dataptr(X), i)
-Base.getindex(X::IDLArray{IDL_STRING}, i::Integer) = IDL_STRING_STR(unsafe_load(_dataptr(X), i))
+
+_rowcolperm(I::NTuple{N, Int}) where {N} = ntuple(N) do j
+	N == 1 && return I[1]
+	j == 1 && return I[2]
+	j == 2 && return I[1]
+	return I[j]
+end
+
+Base.getindex(X::IDLArray, I...) = begin
+	@boundscheck checkbounds(X, I...)
+	@inbounds getindex(X, _rowcolperm(I)...)
+end
+Base.getindex(X::IDLArray, i) = begin
+	@boundscheck checkbounds(X, i)
+	__inbound_getindex(X, i)
+end
+__inbound_getindex(X::IDLArray, i) = unsafe_load(_dataptr(X), i)
+__inbound_getindex(X::IDLArray{IDL_STRING}, i::Integer) = IDL_STRING_STR(unsafe_load(_dataptr(X), i))
 
 
-Base.setindex!(X::IDLArray, v, i) = unsafe_store!(_dataptr(X), v, i)
-Base.setindex!(X::IDLArray{IDL_STRING}, s::AbstractString, i::Integer) =
+
+
+Base.setindex!(X::IDLArray, v, I...) = begin
+	@boundscheck checkbounds(X, I...)
+	@inbounds setindex!(X, v, _rowcolperm(I)...)
+end
+Base.setindex!(X::IDLArray, v, i) = begin
+	@boundscheck checkbounds(X, i)
+	__inbound_setindex!(X, v, i)
+end
+__inbound_setindex!(X::IDLArray, v, i) = unsafe_store!(_dataptr(X), v, i)
+__inbound_setindex!(X::IDLArray{IDL_STRING}, s::AbstractString, i::Integer) =
 	IDL_StrStore(_dataptr(X) + (sizeof(IDL_STRING) * (Int(i) - 1)), s)
+
+
