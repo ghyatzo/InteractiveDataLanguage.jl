93e40d624823278030cf9d10cbc51f17bb07ca36
Camillo Schenone <camillo.schenone@gmail.com>
2025-03-09 17:11:56 +0100

    update scalar tests with new api and condense them.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
 using IDL, Test
 
-execute(str::AbstractString) = IDL.IDL_ExecuteStr(str)
-resetsession() = execute(".reset_session")
 ENV["JULIA_TEST_FAILFAST"] = true
 
-const IDL_SIMPLE_TYPES = [
+const IDL_SCALAR_TYPES = [
 	UInt8 => "B",
 	Int16 => "S",
 	UInt16 => "US",
@@ -14,99 +12,101 @@ const IDL_SIMPLE_TYPES = [
 	UInt64 => "ULL",
 	Float32 => ".0",
 	Float64 => "D",
+	ComplexF32 => "COMPLEX",
+	ComplexF64 => "DCOMPLEX"
 ]
 
 @testset "Scalar Values" begin
 	include("scalar_values.jl")
 end
 
-@testset "Arrays" begin
-	include("arrays.jl")
-end
+# @testset "Arrays" begin
+# 	include("arrays.jl")
+# end
 
-@testset "GET: Structs" begin
-	@testset "Simple Struct" begin
-		simple_idl_struct = "{TAG1:1, TAG2: 2L, TAG3: 3.0D, TAG4: COMPLEX(42, 42)}"
-		IDL.execute("s = $simple_idl_struct")
-		s = IDL.getvar("s")
+# @testset "GET: Structs" begin
+# 	@testset "Simple Struct" begin
+# 		simple_idl_struct = "{TAG1:1, TAG2: 2L, TAG3: 3.0D, TAG4: COMPLEX(42, 42)}"
+# 		IDL.execute("s = $simple_idl_struct")
+# 		s = IDL.getvar("s")
 
-		@test IDL.ntags(s) == 4
-		@test Base.nameof(s) == Symbol()
-		@test IDL.tags(s) == (:TAG1, :TAG2, :TAG3, :TAG4)
+# 		@test IDL.ntags(s) == 4
+# 		@test Base.nameof(s) == Symbol()
+# 		@test IDL.tags(s) == (:TAG1, :TAG2, :TAG3, :TAG4)
 
-		@test s.TAG1 == Int16(1)
-		@test s.TAG2 == Int32(2)
-		@test s.TAG3 == Float64(3)
-		@test s.TAG4 == ComplexF32(42, 42)
+# 		@test s.TAG1 == Int16(1)
+# 		@test s.TAG2 == Int32(2)
+# 		@test s.TAG3 == Float64(3)
+# 		@test s.TAG4 == ComplexF32(42, 42)
 
-		resetsession()
-	end
+# 		resetsession()
+# 	end
 
-	@testset "Named struct with arrays" begin
-		idl_struct_with_array = "{TESTSTRUCT, ARR:[42, 69], ARR2:[1.0, 2.0]}"
+# 	@testset "Named struct with arrays" begin
+# 		idl_struct_with_array = "{TESTSTRUCT, ARR:[42, 69], ARR2:[1.0, 2.0]}"
 
-		IDL.execute("s = $idl_struct_with_array")
-		s = IDL.getvar("s")
+# 		IDL.execute("s = $idl_struct_with_array")
+# 		s = IDL.getvar("s")
 
-		@test IDL.ntags(s) == 2
-		@test Base.nameof(s) == :TESTSTRUCT
-		@test IDL.tags(s) == (:ARR, :ARR2)
-		@test s.ARR isa IDL.IDLArray{Int16, 1}
-		@test s.ARR2 isa IDL.IDLArray{Float32, 1}
+# 		@test IDL.ntags(s) == 2
+# 		@test Base.nameof(s) == :TESTSTRUCT
+# 		@test IDL.tags(s) == (:ARR, :ARR2)
+# 		@test s.ARR isa IDL.IDLArray{Int16, 1}
+# 		@test s.ARR2 isa IDL.IDLArray{Float32, 1}
 
-		@test s.ARR[1] == Int16(42)
-		@test s.ARR[2] == Int16(69)
-		@test s.ARR2[1] == Float32(1)
-		@test s.ARR2[2] == Float32(2)
+# 		@test s.ARR[1] == Int16(42)
+# 		@test s.ARR[2] == Int16(69)
+# 		@test s.ARR2[1] == Float32(1)
+# 		@test s.ARR2[2] == Float32(2)
 
-		resetsession()
-	end
+# 		resetsession()
+# 	end
 
-	@testset "Nested Structs" begin
-		nested_struct = "{OUTER, NESTED:{INNER, TAG1:42, TAG2:[1,2]}}"
-		IDL.execute("s = $nested_struct")
-		s = IDL.getvar("s")
+# 	@testset "Nested Structs" begin
+# 		nested_struct = "{OUTER, NESTED:{INNER, TAG1:42, TAG2:[1,2]}}"
+# 		IDL.execute("s = $nested_struct")
+# 		s = IDL.getvar("s")
 
-		@test IDL.ntags(s) == 1
-		@test Base.nameof(s) == :OUTER
-		@test IDL.tags(s) == (:NESTED,)
-		@test s.NESTED isa
-			IDL.IDLStruct{
-				:INNER,
-				(:TAG1, :TAG2),
-				Tuple{IDL.StructTag{Ptr{Int16}}, IDL.StructTag{IDL.IDLArray{Int16, 1}}},
-				2
-			}
+# 		@test IDL.ntags(s) == 1
+# 		@test Base.nameof(s) == :OUTER
+# 		@test IDL.tags(s) == (:NESTED,)
+# 		@test s.NESTED isa
+# 			IDL.IDLStruct{
+# 				:INNER,
+# 				(:TAG1, :TAG2),
+# 				Tuple{IDL.StructTag{Ptr{Int16}}, IDL.StructTag{IDL.IDLArray{Int16, 1}}},
+# 				2
+# 			}
 
-		ns = s.NESTED
-		@test IDL.ntags(ns) == 2
-		@test nameof(ns) == :INNER
-		@test IDL.tags(ns) == (:TAG1, :TAG2)
-		@test ns.TAG1 == Int16(42)
-		@test ns.TAG2 isa IDL.IDLArray{Int16, 1}
-		@test ns.TAG2[1] == Int16(1)
-		@test ns.TAG2[2] == Int16(2)
+# 		ns = s.NESTED
+# 		@test IDL.ntags(ns) == 2
+# 		@test nameof(ns) == :INNER
+# 		@test IDL.tags(ns) == (:TAG1, :TAG2)
+# 		@test ns.TAG1 == Int16(42)
+# 		@test ns.TAG2 isa IDL.IDLArray{Int16, 1}
+# 		@test ns.TAG2[1] == Int16(1)
+# 		@test ns.TAG2[2] == Int16(2)
 
-		resetsession()
-	end
+# 		resetsession()
+# 	end
 
-	@testset "Array of structs" begin
-		sa_string = "[{A:1, B:2}, {A:4, B:10}]"
-		IDL.execute("s = $sa_string")
-		sa = IDL.getvar("s")
+# 	@testset "Array of structs" begin
+# 		sa_string = "[{A:1, B:2}, {A:4, B:10}]"
+# 		IDL.execute("s = $sa_string")
+# 		sa = IDL.getvar("s")
 
-		@test length(sa) == 2
-		@test typeof(first(sa)) == typeof(last(sa))
-		@test sa[1].A == 1
-		@test sa[1].B == 2
-		@test sa[2].A == 4
-		@test sa[2].B == 10
+# 		@test length(sa) == 2
+# 		@test typeof(first(sa)) == typeof(last(sa))
+# 		@test sa[1].A == 1
+# 		@test sa[1].B == 2
+# 		@test sa[2].A == 4
+# 		@test sa[2].B == 10
 
-		resetsession()
-	end
+# 		resetsession()
+# 	end
 
 
-end
+# end
 
 
 # Structs # (sub sections: GET, PUT)
--- test/scalar_values.jl
+++ test/scalar_values.jl
@@ -1,248 +1,41 @@
+@testset "Scalar Variables" begin
+	@testset for (i, pair) in enumerate(IDL_SCALAR_TYPES)
+		T, Tstr = pair
+		if T == ComplexF32 || T == ComplexF64
+			IDL.execute("v = $Tstr(5, 0)")
+		else
+			IDL.execute("v = 5$Tstr")
+		end
 
-@testset "GET: Scalar Variables" begin
+		v = IDL.var(:v)
+		v2 = IDL.var(:v2, T(5))
 
-	@testset "UInt8" begin
-		execute("var = 5B")
+		@test v[] == T(5)
+		@test v2[] == T(5)
 
-		var = IDL.var(:var)
+		v[] = T(42)
+		@test v[] == T(42)
 
-		@test var isa UInt8
-		@test var == 0x05
+		newT, _ = rand(IDL_SCALAR_TYPES)
+		IDL.set!(v, newT(69))
 
-		resetsession()
-	end
+		@test v[] == newT(69)
 
-	@testset "Int16/UInt16" begin
-		execute("var = 5S")
-		execute("varu = 5US")
-
-		var = IDL.var(:var)
-		varu = IDL.var(:varu)
-
-		@test varu isa UInt16
-		@test varu == UInt16(5)
-
-		@test var isa Int16
-		@test var == Int16(5)
-
-		resetsession()
-	end
-
-	@testset "Int32/UInt32" begin
-		execute("var = 5L")
-		execute("varu = 5UL")
-
-		var = IDL.var(:var)
-		varu = IDL.var(:varu)
-
-		@test varu isa UInt32
-		@test varu == UInt32(5)
-
-		@test var isa Int32
-		@test var == Int32(5)
-
-		resetsession()
-	end
-
-	@testset "Int64/UInt64" begin
-		execute("var = 5LL")
-		execute("varu = 5ULL")
-
-		var = IDL.var(:var)
-		varu = IDL.var(:varu)
-
-		@test varu isa UInt64
-		@test varu == UInt64(5)
-
-		@test var isa Int64
-		@test var == Int64(5)
-
-		resetsession()
-	end
-
-	@testset "Float32" begin
-		execute("var = 5.0")
-
-		var = IDL.var(:var)
-
-		@test var isa Float32
-		@test var == Float32(5)
-
-		resetsession()
-	end
-
-	@testset "Float64" begin
-		execute("var = 5.0D")
-
-		var = IDL.var(:var)
-
-		@test var isa Float64
-		@test var == Float64(5)
-
-		resetsession()
-	end
-
-	@testset "ComplexF32" begin
-		execute("var = COMPLEX(69.0, 42.0)")
-
-		var = IDL.var(:var)
-
-		@test var isa ComplexF32
-		@test var == ComplexF32(69.0, 42.0)
-
-		resetsession()
-	end
-
-	@testset "ComplexF64" begin
-		execute("var = DCOMPLEX(69.0, 42.0)")
-
-		var = IDL.var(:var)
-
-		@test var isa ComplexF64
-		@test var == ComplexF64(69.0, 42.0)
-
-		resetsession()
 	end
 
 	@testset "Strings" begin
-		execute("var = 'Hello!'")
+		IDL.execute("v = 'Hello!'")
 
-		var = IDL.var(:var)
+		v = IDL.var(:v)
 
-		@test var isa String
-		@test var == "Hello!"
-	end
-end
+		@test v[] == "Hello!"
 
-#=======~~~~~~~~~~~~~~~~~~~~~~~~~~======#
+		v[] = "World"
 
-@testset "PUT: Scalar Variables" begin
-	@testset "UInt8" begin
-		var = 0x05
+		@test v[] == "World"
 
-		_var = IDL.var(:var, var)
-		varvar = IDL.var(:var)
+		IDL.set!(v, 10)
 
-		@test varvar isa UInt8
-		@test varvar == var
-
-		resetsession()
-	end
-
-	@testset "Int16/UInt16" begin
-		var = Int16(5)
-		varu = UInt16(5)
-
-		_var = IDL.var(:var, var)
-		_varu = IDL.var(:varu, varu)
-
-		varvar = IDL.var(:var)
-		varvaru = IDL.var(:varu)
-
-		@test varvar isa Int16
-		@test varvar == var
-
-		@test varvaru isa UInt16
-		@test varvaru == varu
-
-		resetsession()
-	end
-
-	@testset "Int32/UInt32" begin
-		var = Int32(5)
-		varu = UInt32(5)
-
-		_var = IDL.var(:var, var)
-		_varu = IDL.var(:varu, varu)
-
-		varvar = IDL.var(:var)
-		varvaru = IDL.var(:varu)
-
-		@test varvar isa Int32
-		@test varvar == var
-
-		@test varvaru isa UInt32
-		@test varvaru == varu
-
-		resetsession()
-	end
-
-	@testset "Int64/UInt64" begin
-		var = Int64(5)
-		varu = UInt64(5)
-
-		_var = IDL.var(:var, var)
-		_varu = IDL.var(:varu, varu)
-
-		varvar = IDL.var(:var)
-		varvaru = IDL.var(:varu)
-
-		@test varvar isa Int64
-		@test varvar == var
-
-		@test varvaru isa UInt64
-		@test varvaru == varu
-
-		resetsession()
-	end
-
-	@testset "Float32" begin
-		var = Float32(5)
-
-		_var = IDL.var(:var, var)
-		varvar = IDL.var(:var)
-
-		@test varvar isa Float32
-		@test varvar == var
-
-		resetsession()
-	end
-
-	@testset "Float64" begin
-		var = Float64(5)
-
-		_var = IDL.var(:var, var)
-		varvar = IDL.var(:var)
-
-		@test varvar isa Float64
-		@test varvar == var
-
-		resetsession()
-	end
-
-	@testset "ComplexF32" begin
-		var = ComplexF32(5)
-
-		_var = IDL.var(:var, var)
-		varvar = IDL.var(:var)
-
-		@test varvar isa ComplexF32
-		@test varvar == var
-
-		resetsession()
-	end
-
-	@testset "ComplexF64" begin
-		var = ComplexF64(5)
-
-		_var = IDL.var(:var, var)
-		varvar = IDL.var(:var)
-
-		@test varvar isa ComplexF64
-		@test varvar == var
-
-		resetsession()
-	end
-
-	@testset "Strings" begin
-		var = "Hello!"
-
-		_var = IDL.var(:var, var)
-		varvar = IDL.var(:var)
-
-		@test varvar isa String
-		@test varvar == var
-
-		resetsession()
+		@test v[] == 10
 	end
 end
