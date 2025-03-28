9ce0e0c85984abbf589539bd2cc9f8d33bbb0142
Camillo Schenone <camillo.schenone@gmail.com>
2025-03-28 20:01:41 +0100

    fix array tests with new changes

--- test/arrays.jl
+++ test/arrays.jl
@@ -1,93 +1,99 @@
-@testset "GET: Arrays of scalars" begin
+# @testset "GET: Arrays of scalars" begin
 
-	@testset for (jltype, idlsuffix) in IDL_SCALAR_TYPES
+# 	@testset for (jltype, idlsuffix) in IDL_SCALAR_TYPES
 
-		if jltype == ComplexF32 || jltype == ComplexF64
-			array_string = "[" * join(fill("$idlsuffix(5, 0)", 5), ',') * "]"
-		else
-			array_string = "[" * join(fill("5$idlsuffix", 5), ',') * "]"
-		end
+# 		if jltype == ComplexF32 || jltype == ComplexF64
+# 			array_string = "[" * join(fill("$idlsuffix(5, 0)", 5), ',') * "]"
+# 		else
+# 			array_string = "[" * join(fill("5$idlsuffix", 5), ',') * "]"
+# 		end
 
-		IDL.execute("var = $array_string")
+# 		idlrun("var = $array_string")
 
-		var = IDL.var(:var)
+# 		var = idlvar(:var)
 
-		@test var[] isa AbstractArray{jltype, 1}
-		@test var[] == jltype[5, 5, 5, 5, 5]
+# 		@test jlarray(var) isa Array{jltype, 1}
+# 		@test length(unsafe_jlview(var)) == 5
 
-		@test var[][1] == jltype(5)
-		var[][2] = jltype(6)
-		@test var[] == jltype[5, 6, 5, 5, 5]
+# 		@test var[] isa AbstractArray{jltype, 1}
+# 		@test var[] == jltype[5, 5, 5, 5, 5]
+
+# 		@test var[][1] == jltype(5)
+# 		var[][2] = jltype(6)
+# 		@test var[] == jltype[5, 6, 5, 5, 5]
+
+# 		arrview = jlview(var)
+# 		idlrun("var = [var, 6]")
+# 		@test_throws InvalidStateException arrview[1]
+# 	end
+
+# 	@testset "Strings" begin
+# 		idlrun("var = ['Hello', 'IDL']")
+
+# 		var = idlvar(:var)
+
+# 		@test jlarray(var) isa Array{String, 1}
+# 		@test length(unsafe_jlview(var)) == 2
+
+# 		@test var[] isa AbstractArray{String, 1}
+# 		@test var[] == ["Hello", "IDL"]
+
+# 		@test var[][1] == "Hello"
+# 		var[][2] = "World"
+# 		@test var[] == ["Hello", "World"]
+
+# 		arrview = jlview(var)
+# 		idlrun("var = [var, 'RIIIR']")
+# 		@test_throws InvalidStateException arrview[1]
+# 	end
+# end
+
+@testset "PUT: Arrays of scalars" begin
+
+	idlrun("var = []")
+	@testset for type in first.(IDL_SCALAR_TYPES)
+	# @testset for type in [Int, UInt]
+		@info type
+		arr = type[1, 2, 3, 4, 5]
+
+		var = idlvar(:var)
+
+		idlarray(:var, arr)
+		@test var[] isa AbstractArray{type, 1}
+		@test all(var[] .== arr)
+
+		sim = idlsimilar(arr)
+		simview = jlview(sim)
+		@test simview isa AbstractArray{type, 1}
+		@test size(sim[]) == size(arr)
+
+		temp = maketemp(arr)
+		@test IDL.istemp(temp)
+		@test all(temp[] .== arr)
+
+		wrap = maketempwrap(arr)
+		@test wrap[] isa AbstractArray{type, 1}
+		@test all(wrap[] .== arr)
+
+		# wrap[][1] = type(10)
+		# @test arr[1] == type(10)
 
 	end
 
 	@testset "Strings" begin
-		IDL.execute("var = ['Hello', 'IDL']")
+		arr = ["Hello", "IDL"]
+		# String don't support wrapping between them,
+		# since they have different memory layouts
 
-		var = IDL.var(:var)
+		sim = idlsimilar(arr)
+		@test sim[] isa AbstractArray{String, 1}
+		@test size(sim[]) == size(arr)
 
-		@test var[] isa AbstractArray{String, 1}
-		@test var[] == ["Hello", "IDL"]
-
-		@test var[][1] == "Hello"
-		var[][2] = "World"
-		@test var[] == ["Hello", "World"]
+		temp = maketemp(arr)
+		@test IDL.istemp(temp)
+		@test all(temp[] .== arr)
 	end
 end
-
-# @testset "PUT: Arrays of scalars" begin
-
-# 	@testset for type in first.(IDL_SIMPLE_TYPES)
-# 		arr = type[1, 2, 3, 4, 5]
-
-# 		_var = IDL.putvar(arr, "var")
-# 		var = IDL.var(:var)
-
-# 		@test var isa AbstractArray{type, 1}
-# 		@test var == arr
-# 		@test var !== arr
-
-# 		resetsession()
-# 	end
-
-# 	@testset "ComplexF32" begin
-# 		arr = ComplexF32[69 + 42im, 42 + 69im]
-
-# 		_var = IDL.putvar(arr, "var")
-# 		var = IDL.var(:var)
-
-# 		@test var isa AbstractArray{ComplexF32, 1}
-# 		@test var == arr
-# 		@test var !== arr
-
-# 		resetsession()
-# 	end
-
-# 	@testset "ComplexF64" begin
-# 		arr = ComplexF64[69 + 42im, 42 + 69im]
-
-# 		_var = IDL.putvar(arr, "var")
-# 		var = IDL.var(:var)
-
-# 		@test var isa AbstractArray{ComplexF64, 1}
-# 		@test var == arr
-# 		@test var !== arr
-
-# 		resetsession()
-# 	end
-
-# 	@testset "Strings" begin
-# 		arr = ["Hello", "IDL"]
-
-# 		_var = IDL.putvar(arr, "var")
-# 		var = IDL.var(:var)
-
-# 		@test var isa AbstractArray{IDL.IDL_STRING, 1}
-# 		@test var == arr
-
-# 		resetsession()
-# 	end
-# end
 
 # @testset "GET: 2-dim arrays" begin
 # 	execute("var = [[5LL], [5LL]]")
