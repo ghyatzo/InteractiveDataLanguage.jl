baf25e3126936b605bda060cb47d78c089e5413f
Camillo Schenone <camillo.schenone@gmail.com>
2025-04-09 11:11:40 +0200

    move generators in their own folder and project

--- gen/generator.jl
+++ gen/generator.jl
@@ -4,12 +4,11 @@ using Clang.Generators
 cd(@__DIR__)
 
 options = load_options(joinpath(@__DIR__, "generator.toml"))
-options["add_record_constructors"] = true
 
 # add compiler flags, e.g. "-DXXXXXXXXX"
 args = get_default_args()  # Note you must call this function firstly and then append your own flags
 
-headers = ["include/idl_export.h"]
+headers = ["../include/idl_export.h"]
 # there is also an experimental `detect_headers` function for auto-detecting top-level headers in the directory
 # headers = detect_headers(clang_dir, args)
 
